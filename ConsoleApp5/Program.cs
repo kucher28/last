9f78f574a50ebd33b3c2ef6d08601c91a9950fe3
Андрей Кучеренко <kucherenko_andrey_2002@mail.ru>
2020-06-18 16:21:52 +0300

    first commit

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Email");
+            string address = Console.ReadLine();
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            MailAddress from = new MailAddress(address, name);
+
+            Console.WriteLine("Введите Email получателя");
+            string addressPoluch = Console.ReadLine();
+            MailMessage objectMail = new MailMessage(address, addressPoluch);
+
+            Console.WriteLine("Введите тему сообщения");
+            string tema = Console.ReadLine();
+            objectMail.Subject = tema;
+
+            Console.WriteLine("Ввдедите текст сообщения:");
+            string text = Console.ReadLine();
+            objectMail.Body = text;
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+
+            Console.WriteLine("Введите gmail");
+            string gmail = Console.ReadLine();
+            Console.WriteLine("Введите пароль");
+            string password = Console.ReadLine();
+
+            smtp.Credentials = new NetworkCredential(gmail, password);
+            smtp.EnableSsl = true;
+            smtp.Send(objectMail);
+            Console.Read();
+        }
+    }
+}
